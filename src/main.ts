3ebe329a7acc5579a67b4967683fbbed26ecea77
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-06-14 14:44:27 -0400

    commit

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
+  console.log('hola mundo');
 
   logger.log(`Running on port ${envs.port}`);
 }
